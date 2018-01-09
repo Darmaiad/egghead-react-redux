42ee949cf90d0b097ea20a491c59015a7303f590
Filippakis George <g.filippakis@intralot.com>
2018-01-09 13:37:44 +0200

    Simple applyMiddleware implementation

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,26 +5,39 @@ import reducer from './reducers';
 
 // By default Reduc demands that action are plain objects
 const addPromiseSupportToDispatch = (store) => {
-    const rawDispatch = store.dispatch;
-    // We don't know if any given action is a normal action or a async one
-    return (action) => {
-        // Recognize a promise:
-        if (typeof action.then === 'function') {
-            // If it is a promise, we wait for it to resolve to an action object and then we pass it to rawDispatch
-            return action.then(rawDispatch);
-        }
-        // Otherwise we call rawDispatch right away with the object we received
-        return rawDispatch(action);
+    // We name the previous 'rewDispatch' as 'next' because this is the next dispatch function
+    // after the previous middleware was applied (possibly was applied, it is not a given)
+    return (next) => {
+        // We don't know if any given action is a normal action or a async one
+        return (action) => {
+            // Recognize a promise:
+            if (typeof action.then === 'function') {
+                // If it is a promise, we wait for it to resolve to an action object and then we pass it to next
+                return action.then(next);
+            }
+            // Otherwise we call next right away with the object we received
+            return next(action);
+        };
     };
 };
 
+const wrapDispatchWithMiddleWares = (store, middlewares) => {
+    middlewares.forEach((middleware) => {
+        store.dispatch = middleware(store)(store.dispatch);
+    });
+};
+
 const configureStore = () => {
+    const middlewares = [];
+
     const store = createStore(
         reducer,
         applyMiddleware(logger)
     );
 
-    store.dispatch = addPromiseSupportToDispatch(store);
+    middlewares.push(addPromiseSupportToDispatch);
+
+    wrapDispatchWithMiddleWares(store, middlewares);
 
     return store;
 };
